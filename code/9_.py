03fe9dfb5796d0c10bc29468e19466f32c649a64
networkenforcer <networkenforcer@gmail.com>
2023-12-14 21:10:00 +0100

    9

---
+++ code/9_.py
@@ -0,0 +1,37 @@
+import sys
+
+def nextline(line):
+
+	return [line[i+1] - line[i] for i in range(len(line)-1)]
+
+def full_history(line):
+
+	if(all([n == 0 for n in line])):
+		return 0
+
+	return line[0] - full_history(nextline(line))
+
+
+
+if __name__ == "__main__":
+
+	path = sys.argv[1]
+
+	inputs = []
+
+	with open(path, "r") as file:
+
+		inputs = file.read().splitlines()
+
+	inputs = [[int(x) for x in el.split(" ")] for el in inputs]	
+	
+	fullsum = 0
+
+	for el in inputs:
+
+		fullsum += full_history(el)
+		
+
+
+
+	print(fullsum)
